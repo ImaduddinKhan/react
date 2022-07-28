3330eeb9d3000b7415ea1805dc3b6a39c7dce4ae
ImadUdDin <emaduddin181@gmail.com>
2022-07-28 07:02:08 +0500

    adding arrow function syntax

--- src/react-guide/src/App.js
+++ src/react-guide/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e0",
@@ -52,6 +52,6 @@ function App() {
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
