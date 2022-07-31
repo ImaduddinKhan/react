3fdb7329fa18578cc3e06bf947d42d336597abd7
ImadUdDin <emaduddin181@gmail.com>
2022-07-31 15:44:15 +0500

    rendering list of data and adding data dynamically

--- src/react-guide/src/components/Expenses/Expenses.js
+++ src/react-guide/src/components/Expenses/Expenses.js
@@ -19,26 +19,13 @@ const Expenses = (props) => {
           selected={yearFiltered}
           onFilterExpenses={filterExpensesHandler}
         />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {props.expenseItems.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
