4b24c4e467de96e09d2b7bb179f2d8aafad4663a
ImadUdDin <emaduddin181@gmail.com>
2022-08-12 17:05:21 +0500

    adding modal via portals

---
+++ src/demo-reactmeal-app/src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Handi", amount: 2, price: 249.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div clasName={classes.total}>
+        <span>Total Amount</span>
+        <span>35.23</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
