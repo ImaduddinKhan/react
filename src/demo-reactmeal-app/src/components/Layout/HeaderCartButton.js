cadba88c297cfef2ae0e0d23c615608142f5a831
ImadUdDin <emaduddin181@gmail.com>
2022-08-14 02:01:53 +0500

    adding central context provider comp

--- src/demo-reactmeal-app/src/components/Layout/HeaderCartButton.js
+++ src/demo-reactmeal-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItemsOnCart = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.openCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span> Your Cart </span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItemsOnCart}</span>
     </button>
   );
 };
