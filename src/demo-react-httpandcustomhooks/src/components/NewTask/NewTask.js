edb3df57c643f376b15a9a7ed33a62bca58e06ea
ImadUdDin <emaduddin181@gmail.com>
2022-08-27 23:26:20 +0500

    calling useHttp hook for sending post request

--- src/demo-react-httpandcustomhooks/src/components/NewTask/NewTask.js
+++ src/demo-react-httpandcustomhooks/src/components/NewTask/NewTask.js
@@ -1,42 +1,30 @@
-import React, { useState } from "react";
-
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isloading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendPostReq } = useHttp();
+
+  const createTask = (enteredTaskText, taskData) => {
+    const generatedId = taskData.name;
+
+    const createdTask = { id: generatedId, text: enteredTaskText };
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (enteredTaskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(process.env.REACT_APP_API_KEY, {
+    sendPostReq(
+      {
+        url: process.env.REACT_APP_API_KEY,
         method: "POST",
-        body: JSON.stringify({ text: enteredTaskText }),
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name;
-
-      const createdTask = { id: generatedId, text: enteredTaskText };
-      props.onAddTask(createdTask);
-      console.log(createdTask);
-    } catch (error) {
-      setError(error.message || "Something went wrong");
-    }
-    setIsLoading(false);
+        headers: { "content-type": "applicaiton/json" },
+        body: { text: enteredTaskText },
+      },
+      createTask.bind(null, enteredTaskText)
+    );
   };
   return (
     <div>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isloading} />
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
       {error && <p>{error}</p>}
     </div>
   );
