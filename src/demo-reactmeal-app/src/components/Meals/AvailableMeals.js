0cd41598fd9f965eae334375085f217bdf880910
ImadUdDin <emaduddin181@gmail.com>
2022-08-30 16:51:23 +0500

    setting up firebase and handling http errors

--- src/demo-reactmeal-app/src/components/Meals/AvailableMeals.js
+++ src/demo-reactmeal-app/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,90 @@
+import React, { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Handi",
-    description: "White and red tasty handi",
-    price: 249.99,
-  },
-  {
-    id: "m2",
-    name: "Shahi daal",
-    description: "Spicy shahi dall with extra SAALAN",
-    price: 199.99,
-  },
-  {
-    id: "m3",
-    name: "Chicken karai",
-    description: "Full chiken karai with extra wings",
-    price: 299.99,
-  },
-  {
-    id: "m4",
-    name: "Sikh kebab",
-    description: "Four finest sikh kebab per meal",
-    price: 299.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Handi",
+//     description: "White and red tasty handi",
+//     price: 249.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Shahi daal",
+//     description: "Spicy shahi dall with extra SAALAN",
+//     price: 199.99,
+//   },
+//   {
+//     id: "m3",
+//     name: "Chicken karai",
+//     description: "Full chiken karai with extra wings",
+//     price: 299.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Sikh kebab",
+//     description: "Four finest sikh kebab per meal",
+//     price: 299.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-46ac2-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const resData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in resData) {
+        loadedMeals.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals().catch((err) => {
+      setIsLoading(false);
+      setHttpError(err.message);
+      console.log(err.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsIsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
