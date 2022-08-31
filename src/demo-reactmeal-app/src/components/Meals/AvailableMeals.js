dfa00b948565fcc9fbdf69fc17cc472df360ff44
ImadUdDin <emaduddin181@gmail.com>
2022-08-31 15:21:59 +0500

    adding firebase, error handling, checkout from skeleton

--- src/demo-reactmeal-app/src/components/Meals/AvailableMeals.js
+++ src/demo-reactmeal-app/src/components/Meals/AvailableMeals.js
@@ -4,33 +4,6 @@ import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Handi",
-//     description: "White and red tasty handi",
-//     price: 249.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Shahi daal",
-//     description: "Spicy shahi dall with extra SAALAN",
-//     price: 199.99,
-//   },
-//   {
-//     id: "m3",
-//     name: "Chicken karai",
-//     description: "Full chiken karai with extra wings",
-//     price: 299.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Sikh kebab",
-//     description: "Four finest sikh kebab per meal",
-//     price: 299.99,
-//   },
-// ];
-
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
