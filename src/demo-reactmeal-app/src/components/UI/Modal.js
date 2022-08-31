85c554785bd992d74f666132e52c31730a2891db
ImadUdDin <emaduddin181@gmail.com>
2022-08-31 08:54:42 +0500

    adding checkout form skeleton

--- src/demo-reactmeal-app/src/components/UI/Modal.js
+++ src/demo-reactmeal-app/src/components/UI/Modal.js
@@ -19,7 +19,7 @@ const Modal = (props) => {
   return (
     <Fragment>
       {ReactDOM.createPortal(
-        <Backdrop onBackDrop={props.onClose} />,
+        <Backdrop onBackDrop={props.onBackdropClose} />,
         portalElement
       )}
       {ReactDOM.createPortal(
