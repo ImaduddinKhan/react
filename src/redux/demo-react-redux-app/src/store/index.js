650c68239141cca77b82cea0aa0b9058327321eb
ImadUdDin <emaduddin181@gmail.com>
2022-09-04 00:16:29 +0500

    replacing redux/toolkit with redux

--- src/redux/demo-react-redux-app/src/store/index.js
+++ src/redux/demo-react-redux-app/src/store/index.js
@@ -1,48 +1,29 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
+const globalCounterSlice = createSlice({
+  name: "counter",
+  initState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-  return state;
-};
-
-const store = createStore(counterReducer);
-// console.log(store.getState());
-
-// const counterSubscriber = () => {
-//   const latestState = store.getState();
-//   console.log(latestState);
-// };
-// store.subscribe(counterSubscriber);
-
-// store.dispatch({ type: "increment" });
-// store.dispatch({ type: "decrement" });
+// const store = createStore(globalCounterSlice.reducers);
+const store = configureStore({
+  reducer: globalCounterSlice.reducer,
+});
 
 export default store;
