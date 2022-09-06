64c783de27bf84d135d903dd16166f3b62d21727
ImadUdDin <emaduddin181@gmail.com>
2022-09-06 19:41:55 +0500

    adding slices reduxjs/toolkit

--- src/redux/demo-app-redux-advanced/src/components/Cart/CartButton.js
+++ src/redux/demo-app-redux-advanced/src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
+import React, { useDispatch } from "react";
+
 import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
