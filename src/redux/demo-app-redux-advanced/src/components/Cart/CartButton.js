9d96e06a19c7107e0b5f498913c9ea05f4746808
ImadUdDin <emaduddin181@gmail.com>
2022-09-07 16:32:11 +0500

    adding and removing data using redux/toolkit

--- src/redux/demo-app-redux-advanced/src/components/Cart/CartButton.js
+++ src/redux/demo-app-redux-advanced/src/components/Cart/CartButton.js
@@ -1,10 +1,11 @@
-import React, { useDispatch } from "react-redux";
+import React, { useDispatch, useSelector } from "react-redux";
 
 import classes from "./CartButton.module.css";
 import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartItemQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
@@ -13,7 +14,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItemQuantity}</span>
     </button>
   );
 };
