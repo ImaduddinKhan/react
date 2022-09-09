8348d02363df9b6bff2bfede5701fd2d4baafe35
ImadUdDin <emaduddin181@gmail.com>
2022-09-10 00:08:27 +0500

    adding react-routes and setting app skeleton

--- src/demo-react-router/src/App.js
+++ src/demo-react-router/src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div className="App">
-     <h1>Bismillah</h1> 
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/demo-react-router/src/pages/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = () => {
+    return <h1>Bismillah, The Welcome Page</h1> 
+}
+
+export default Welcome;
