15c79953b47d4cdea403d5b136af7a903dcd0233
ImadUdDin <emaduddin181@gmail.com>
2022-09-11 15:16:13 +0500

    adding layout and more

--- src/demo-reactrouter-routing/src/components/quotes/QuoteItem.js
+++ src/demo-reactrouter-routing/src/components/quotes/QuoteItem.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import classes from "./QuoteItem.module.css";
 
 const QuoteItem = (props) => {
@@ -9,7 +10,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className="btn">View Fullscreen</a>
+      <Link to={`/quotes/${props.id}`} className="btn">
+        View Fullscreen
+      </Link>
     </li>
   );
 };
--- src/demo-reactrouter-routing/src/pages/QutoeDetail.js
+++ src/demo-reactrouter-routing/src/pages/QutoeDetail.js
@@ -2,14 +2,31 @@ import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Test",
+    text: "Learning is so cool..",
+  },
+  {
+    id: "q2",
+    author: "Test 2",
+    text: "Learning 2 is so cool..",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
