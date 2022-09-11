2f4002acb5f6bfd21eb665596881ccc48f381fc1
ImadUdDin <emaduddin181@gmail.com>
2022-09-12 04:00:25 +0500

    adding query params and nested routes

--- src/demo-reactrouter-routing/src/pages/QutoeDetail.js
+++ src/demo-reactrouter-routing/src/pages/QutoeDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -27,6 +27,13 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
