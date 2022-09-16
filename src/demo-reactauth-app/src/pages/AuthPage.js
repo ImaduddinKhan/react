a4d8c1bea99d58f027c0e296398acd5f1f263061
ImadUdDin <emaduddin181@gmail.com>
2022-09-16 22:52:10 +0500

    skeleton

---
+++ src/demo-reactauth-app/src/pages/AuthPage.js
@@ -0,0 +1,9 @@
+const AuthPage = (props) => {
+  return (
+    <div>
+      <h1>Auth Page</h1>
+    </div>
+  );
+};
+
+export default AuthPage;
---
+++ src/demo-reactauth-app/src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import StartingPage from "../components/StartingPage/StartingPageContent";
+
+const HomePage = (props) => {
+  return <StartingPage />;
+};
+
+export default HomePage;
