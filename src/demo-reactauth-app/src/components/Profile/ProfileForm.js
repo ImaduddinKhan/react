79cd6a14bfbf7b1a6ab7e74f9a563e012c4d00a7
ImadUdDin <emaduddin181@gmail.com>
2022-09-18 00:47:12 +0500

    adding firebase auth

--- src/demo-reactauth-app/src/components/Profile/ProfileForm.js
+++ src/demo-reactauth-app/src/components/Profile/ProfileForm.js
@@ -1,11 +1,47 @@
+import React, { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+
 import classes from "./ProfileForm.module.css";
 
 const Layout = (props) => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+
+  const submitHandler = (eve) => {
+    eve.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const updatePas = process.env.REACT_APP_UPDATEPAS;
+
+    fetch(`${updatePas}${apiKey}`, {
+      method: "POST",
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        "Content-Type": "appliction/json",
+      },
+    }).then((res) => {
+      history.replace("/");
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input id="new-password" type="password" />
+        <input
+          id="new-password"
+          type="password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
