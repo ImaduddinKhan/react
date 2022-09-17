63463a41877bb19aef385673c2ddc4bf1d029ecd
ImadUdDin <emaduddin181@gmail.com>
2022-09-18 00:47:12 +0500

    adding firebase auth

--- src/demo-reactauth-app/src/components/Auth/AuthForm.js
+++ src/demo-reactauth-app/src/components/Auth/AuthForm.js
@@ -1,9 +1,15 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = (props) => {
+  const authCtx = useContext(AuthContext);
+
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const history = useHistory();
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoding, setIsLoding] = useState(false);
@@ -23,38 +29,48 @@ const AuthForm = (props) => {
     console.log("Email:" + enteredEmail + " Password:" + enteredPassword);
 
     setIsLoding(true);
+    let url;
+    const apiKey = `${process.env.REACT_APP_API_KEY}`;
+    const signIn = `${process.env.REACT_APP_SIGNIN}`;
+    const signUp = `${process.env.REACT_APP_SIGNUP}`;
 
     if (isLogin) {
+      url = `${signIn}${apiKey}`;
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDOZcbaNm79uMiGF3BizVSsU31Ey0zVOYY",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: { "Content-Type": "application/json" },
-        }
-      ).then((res) => {
+      url = `${signUp}${apiKey}`;
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
         setIsLoding(false);
 
         if (res.ok) {
-          //...
+          return res.json();
         } else {
-          res.json().then((resData) => {
+          return res.json().then((resData) => {
             let errMessage = "Authentication failed";
 
-            if (resData && resData.error && resData.error.message) {
-              errMessage = resData.error.message;
-            }
-            alert(errMessage);
+            // if (resData && resData.error && resData.error.message) {
+            //   errMessage = resData.error.message;
+            // }
+            throw new Error(errMessage);
           });
         }
+      })
+      .then((data) => {
+        authCtx.login(data.idToken);
+        history.replace("/");
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
-
     // setEnteredEmail("");
     // setEnteredPassword("");
   };
--- src/demo-reactauth-app/src/components/Layout/MainNavigation.js
+++ src/demo-reactauth-app/src/components/Layout/MainNavigation.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = (props) => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -9,15 +19,22 @@ const MainNavigation = (props) => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to="/auth">Login</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
