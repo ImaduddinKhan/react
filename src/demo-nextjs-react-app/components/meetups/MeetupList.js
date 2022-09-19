b410b61d7360e1eb2fd1efe306b31df947927e7a
ImadUdDin <emaduddin181@gmail.com>
2022-09-19 08:19:47 +0500

    setting up and cascading

--- src/demo-nextjs-react-app/components/meetups/MeetupList.js
+++ src/demo-nextjs-react-app/components/meetups/MeetupList.js
@@ -1,7 +1,20 @@
-import classes from "./MeetUpItem.module.css";
+import MeetupItem from "./MeetUpItem";
+import classes from "./MeetUpList.module.css";
 
 const MeetUpItem = (props) => {
-  return <p>Meetup List page</p>;
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          address={meetup.address}
+          discription={meetup.discription}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default MeetUpItem;
