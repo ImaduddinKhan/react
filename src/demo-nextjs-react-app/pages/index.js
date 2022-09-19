c959e792313175a8ac195de2024ad38a99ae84bb
ImadUdDin <emaduddin181@gmail.com>
2022-09-19 08:24:55 +0500

    setting up and cascading

--- src/demo-nextjs-react-app/pages/index.js
+++ src/demo-nextjs-react-app/pages/index.js
@@ -1,9 +1,31 @@
-import styles from '../styles/Home.module.css'
+import { Fragment } from "react";
+import styles from "../styles/Home.module.css";
+import MeetupList from "../components/meetups/MeetupList";
 
-export default function Home() {
+const DUMMY_MEETUP_LIST = [
+  {
+    id: "m1",
+    title: "A Meetup",
+    image:
+      "https://images.unsplash.com/photo-1598908314732-07113901949e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    address: "Some address Lane 3, House 338",
+    description: "This is my first meetup",
+  },
+  {
+    id: "m2",
+    title: "An another Meetup",
+    image:
+      "https://images.unsplash.com/photo-1598908314732-07113901949e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    address: "Some address Lane 3, House 338-A",
+    description: "This is my second meetup",
+  },
+];
+
+function HomePage() {
   return (
-    <div className={styles.container}>
-      
-    </div>
-  )
+    <Fragment>
+      <MeetupList meetups={DUMMY_MEETUP_LIST} />
+    </Fragment>
+  );
 }
+export default HomePage;
---
+++ src/demo-nextjs-react-app/pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetUpPage = () => {
+  const addMeetupHandler = () => {};
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetUpPage;
