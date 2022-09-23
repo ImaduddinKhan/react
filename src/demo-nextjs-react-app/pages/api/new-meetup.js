0dfd504af5b354b6fc3907f6a1297f14ee6a21d5
ImadUdDin <emaduddin181@gmail.com>
2022-09-23 09:36:56 +0500

    connecting with mongodb & sending post request

--- src/demo-nextjs-react-app/pages/api/new-meetup.js
+++ src/demo-nextjs-react-app/pages/api/new-meetup.js
@@ -1,9 +1,26 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-const handler = (req, res) => {
-  if (req === "POST") {
+const handler = async (req, res) => {
+  if (req.method === "POST") {
     const data = req.body;
+
+    const client = await MongoClient.connect(
+      process.env.REACT_APP_API_MONGOCLIENT
+    );
+    console.log(client);
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "New meetups inserted!" });
   }
 };
 
