49694afcb5899b22430b2fb104b77f2308e74cd1
ImadUdDin <emaduddin181@gmail.com>
2022-09-28 01:06:09 +0500

    adding context api while replacing redux

--- src/demo-replacing-redux/src/index.js
+++ src/demo-replacing-redux/src/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 import productReducer from "./store/reducers/products";
-
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
-
-const store = createStore(rootReducer);
+import PorductsProvider from "./context/products-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider store={store}>
+  <PorductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  </PorductsProvider>
 );
